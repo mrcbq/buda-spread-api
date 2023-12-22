d982c0bdfe60a4eb003ca11b86155e0d3494875e
mrcbq <marcobaquero@gmail.com>
2023-12-22 02:27:51 -0500

    Change order book for ticker to calculate the spreads

--- app/controllers/api/v1/spreads_controller.rb
+++ app/controllers/api/v1/spreads_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::SpreadsController < ApplicationController
   def sanitize_params
     market = params[:id]
     active_markets = get_active_markets
-    
+
     unless active_markets.include?(market)
       render json: { error: 'Invalid market' }, status: :unprocessable_entity
     end
@@ -30,15 +30,15 @@ class Api::V1::SpreadsController < ApplicationController
     markets.map { |market| market['id'] unless market['disabled'] }.compact
   end
   
-  def get_order_book(market)
-    response = HTTParty.get("https://www.buda.com/api/v2/markets/#{market}/order_book")
+  def get_ticker(market)
+    response = HTTParty.get("https://www.buda.com/api/v2/markets/#{market}/ticker")
     order_book = JSON.parse(response.body) #hash
   end
 
   def calculate_spread(market)
-    order_book = get_order_book(market)
-    lower_ask = order_book['order_book']['asks'][0][0].to_f
-    higher_bid = order_book['order_book']['bids'][0][0].to_f
+    order_book = get_ticker(market)
+    lower_ask = order_book['ticker']['min_ask'][0].to_f
+    higher_bid = order_book['ticker']['max_bid'][0].to_f
     spread = lower_ask - higher_bid
     { market: market, spread: spread }
   end
